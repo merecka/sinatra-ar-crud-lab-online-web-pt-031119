d102fafac62bcdd604d7e3dc2af4ecf2614344d3
Alex Merecka <merecka@gmail.com>
2019-06-21 01:01:56 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+  attr_reader :title, :content
+
+  def initialize(title, content)
+    @title = title
+    @content = content
+  end
+  
 end
